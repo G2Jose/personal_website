315db7ef6dd4fc2f9af57dd8b149e6bf510d789a
George Jose <george@earnwithdrop.com>
2023-02-24 21:51:44 -0500

    Update favicon

--- web/gatsby-config.ts
+++ web/gatsby-config.ts
@@ -113,15 +113,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `Gatsby`,
+        name: `George's Personal Blog`,
+        short_name: `george-personal-blog`,
         start_url: `/`,
         background_color: `#ffffff`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
+        theme_color: `#000`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/profile-pic.png`,
       },
     },
   ],
