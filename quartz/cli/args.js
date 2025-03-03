e4c5101eee60673a6ac1ee842532ebd71d67d63f
George Jose <george.jose.a@gmail.com>
2025-03-03 18:08:42 -0500

    stuff

--- quartz/cli/args.js
+++ quartz/cli/args.js
@@ -63,7 +63,7 @@ export const BuildArgv = {
   output: {
     string: true,
     alias: ["o"],
-    default: "public",
+    default: "docs",
     describe: "output folder for files",
   },
   serve: {
