5f28c5a861255c184a1e6266761ee0ff89047e6e
George Jose <george.jose.a@gmail.com>
2025-03-14 00:10:11 -0400

    Updates

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "georgejose.com",
+    baseUrl: "www.georgejose.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
